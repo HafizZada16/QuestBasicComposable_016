8eacbdea4ad6b946922af2f8b80e2f69d0d46eae
HafizZada16 <hafizzada16@gmail.com>
2025-10-17 15:29:58 +0700

    Membuat TataLetakColumnRow

--- app/src/main/java/com/example/hapizapp/TataLetak.kt
+++ app/src/main/java/com/example/hapizapp/TataLetak.kt
@@ -1,12 +1,15 @@
 package com.example.hapizapp
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -21,12 +24,27 @@ fun TataLetakColumn(modifier: Modifier) {
 }
 
 @Composable
-fun TataletakRow(modifier: Modifier) {
+fun TataLetakRow(modifier: Modifier) {
     Row(modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
         Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataLetakColumnRow(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column2")
     }
 }

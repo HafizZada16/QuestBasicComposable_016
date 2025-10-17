de23a6afd0545d65625885e96454d28fd53a9cc0
HafizZada16 <hafizzada16@gmail.com>
2025-10-17 16:11:23 +0700

    Membuat TataLetakBoxColumnRow bagian Spacer dan Box

--- app/src/main/java/com/example/hapizapp/TataLetak.kt
+++ app/src/main/java/com/example/hapizapp/TataLetak.kt
@@ -1,5 +1,6 @@
 package com.example.hapizapp
 
+import android.text.Layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,12 +11,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -124,7 +127,11 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
         }
         Spacer(modifier = modifier.height(10.dp))
         Box(
-            mo
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan ),
+            contentAlignment = Alignment.Center
         )
     }
 }
